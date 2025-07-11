4c27470ccf0507bc48a1e80da696befda46faf22
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-07-11 10:02:57 +0530

    use webclient to access the full ClientResponse

--- order-service/src/main/java/com/mbpt/eazycart/microservices/order_service/util/InterServiceCommunicationHandler.java
+++ order-service/src/main/java/com/mbpt/eazycart/microservices/order_service/util/InterServiceCommunicationHandler.java
@@ -1,14 +1,10 @@
 package com.mbpt.eazycart.microservices.order_service.util;
 
-import com.mbpt.eazycart.microservices.order_service.dto.ProductDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import java.util.List;
 
 @Component
 public class InterServiceCommunicationHandler {
@@ -20,12 +16,11 @@ public class InterServiceCommunicationHandler {
         this.webClient = webClient;
     }
 
-   public <T> T interServiceCallByWebClient(String url, ParameterizedTypeReference<T> typeRef) {
-    return webClient.get()
-            .uri(url)
-            .retrieve()
-            .bodyToMono(typeRef)
-            .block();
-}
+    public <T> ResponseEntity<T> interServiceCallByWebClient(String url, ParameterizedTypeReference<T> typeRef) {
+        return webClient.get()
+                .uri(url)
+                .exchangeToMono(response -> response.toEntity(typeRef))
+                .block();
+    }
 
 }
--- order-service/src/main/java/com/mbpt/eazycart/microservices/order_service/util/ProductClient.java
+++ order-service/src/main/java/com/mbpt/eazycart/microservices/order_service/util/ProductClient.java
@@ -1,6 +1,5 @@
 package com.mbpt.eazycart.microservices.order_service.util;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mbpt.eazycart.microservices.order_service.dto.ProductDTO;
 import com.mbpt.eazycart.microservices.order_service.exception.InvalidOrderItemsException;
 import com.mbpt.eazycart.microservices.order_service.exception.ProductServiceException;
@@ -9,9 +8,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
-import java.net.http.HttpResponse;
 import java.util.List;
 
 @Component
@@ -20,39 +19,36 @@ public class ProductClient {
     @Value("${product.service.url}")
     private String productServiceUrl;
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
     private static final Logger logger = LoggerFactory.getLogger(ProductClient.class);
 
     @Autowired
     private InterServiceCommunicationHandler interServiceCommunicationHandler;
 
     public List<ProductDTO> getAllProducts() {
-        final HttpResponse<List<ProductDTO>> response = interServiceCommunicationHandler
-                .interServiceCallByWebClient(productServiceUrl + "/all", new ParameterizedTypeReference<HttpResponse<List<ProductDTO>>>() {
+        final ResponseEntity<List<ProductDTO>> response = interServiceCommunicationHandler
+                .interServiceCallByWebClient(productServiceUrl + "/all", new ParameterizedTypeReference<List<ProductDTO>>() {
                 });
-        if (response.statusCode() == 200) {
-            return response.body();
+        if (response.getStatusCode().value() == 200) {
+            return response.getBody();
         } else {
-            String errorMessage = String.format("Product service returned status %d for /all. Response: %s", response.statusCode(), response.body());
+            String errorMessage = String.format("Product service returned status %d for /all. Response: %s", response.getStatusCode().value(), response.getBody());
             logger.error(errorMessage);
             throw new ProductServiceException(errorMessage);
         }
     }
 
     public ProductDTO findProductById(Integer product_id) {
-        HttpResponse<ProductDTO> response = interServiceCommunicationHandler
-                .interServiceCallByWebClient(productServiceUrl + "/" + product_id, new ParameterizedTypeReference<HttpResponse<ProductDTO>>() {
+        ResponseEntity<ProductDTO> response = interServiceCommunicationHandler
+                .interServiceCallByWebClient(productServiceUrl + "/" + product_id, new ParameterizedTypeReference<ProductDTO>() {
                 });
-        if (response.statusCode() == 200) {
-            return response.body();
-        } else if (response.statusCode() == 404) {
-            String errorMessage = String.format("Product not found (status %d) for ID %d. Response: %s", response.statusCode(), product_id, response.body());
+        if (response.getStatusCode().value() == 200) {
+            return response.getBody();
+        } else if (response.getStatusCode().value() == 404) {
+            String errorMessage = String.format("Product not found (status %d) for ID %d. Response: %s", response.getStatusCode().value(), product_id, response.getBody());
             logger.error(errorMessage);
             throw new InvalidOrderItemsException(errorMessage);
         } else {
-            String errorMessage = String.format("Product service returned status %d for ID %d. Response: %s", response.statusCode(), product_id, response.body());
+            String errorMessage = String.format("Product service returned status %d for ID %d. Response: %s", response.getStatusCode().value(), product_id, response.getBody());
             logger.error(errorMessage);
             throw new ProductServiceException(errorMessage);
         }
